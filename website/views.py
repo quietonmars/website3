1eb8c73135ed226756b2d9ca533952d7da09a878
quietonmars <quietonmars@gmail.com>
2023-02-20 12:10:13 +0630

    Initial Commit

--- website/views.py
+++ website/views.py
@@ -18,6 +18,16 @@ def home():
     category_list = {}
     department_list = {}
 
+    likes = Like.query.filter_by(status=1).all()
+    dislikes = Like.query.filter_by(status=-1).all()
+    like_list = {}
+    dislike_list = {}
+
+    for lik in likes:
+        like_list[lik.idea_id] = lik.staff_id
+
+    for dislik in dislikes:
+        dislike_list[dislik.idea_id] = dislik.staff_id
 
     for cat in categories:
         category_list[cat.id] = cat.name
@@ -29,27 +39,54 @@ def home():
         idea_id = request.form['idea_id']
         status = request.form.get('status')
         staff_id = current_user.id
-        print("idea: " + idea_id,"status:", status,"staff_id:", staff_id)
         idea = Ideas.query.get(idea_id)
-        if status == "1":
-            idea.like += 1
-            like = Like(idea_id=idea_id, staff_id=staff_id, status=status)
-            db.session.add(like)
-            db.session.commit()
-            flash('Idea liked successfully', category='success')
+        like = Like.query.filter_by(idea_id=idea_id, staff_id=staff_id).first()
 
-
-        elif status == "-1":
-            idea.dislike += 1
-            dislike = Like(idea_id=idea_id, staff_id=staff_id, status=status)
-            db.session.add(dislike)
-            db.session.commit()
-            flash('Idea disliked successfully', category='success')
-
+        if like:
+            # The user has already liked or disliked the idea
+            if like.status == 1 and status == '1':
+                # The user has already liked the idea and clicks the like button again, so remove the like
+                idea.like -= 1
+                db.session.delete(like)
+                flash('Idea like removed', category='success')
+            elif like.status == -1 and status == '-1':
+                # The user has already disliked the idea and clicks the dislike button again, so remove the dislike
+                idea.dislike -= 1
+                db.session.delete(like)
+                flash('Idea dislike removed', category='success')
+            elif like.status == 1 and status == '-1':
+                # The user has already liked the idea and clicks the dislike button, so remove the like and add the dislike
+                idea.like -= 1
+                idea.dislike += 1
+                like.status = -1
+                flash('Idea disliked successfully', category='success')
+            elif like.status == -1 and status == '1':
+                # The user has already disliked the idea and clicks the like button, so remove the dislike and add the like
+                idea.dislike -= 1
+                idea.like += 1
+                like.status = 1
+                flash('Idea liked successfully', category='success')
         else:
-            flash('error', category='danger')
+            # The user has not yet liked or disliked the idea
+            if status == '1':
+                # The user likes the idea
+                idea.like += 1
+                like = Like(idea_id=idea_id, staff_id=staff_id, status=1)
+                db.session.add(like)
+                flash('Idea liked successfully', category='success')
+            elif status == '-1':
+                # The user dislikes the idea
+                idea.dislike += 1
+                like = Like(idea_id=idea_id, staff_id=staff_id, status=-1)
+                db.session.add(like)
+                flash('Idea disliked successfully', category='success')
+            else:
+                flash('error', category='danger')
+
+        db.session.commit()
 
         return redirect(url_for('views.home'))
 
     return render_template('staff_home.html', ideas=ideas, user=current_user,
-                           categories=category_list, departments=department_list)
+                           categories=category_list, departments=department_list, like_list=like_list, dislike_list=dislike_list)
+
